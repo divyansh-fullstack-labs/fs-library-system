6c0b6ac2b1f00c9749bf602d8eabb7afc63e4400
Divyansh-Sinha <divyansh8252230@gmail.com>
2026-06-03 10:16:46 +0530

    initial commit for repo

---
+++ backend/src/test/java/com/divyansh/fullstacklabs/librarySystem/LibrarySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divyansh.fullstacklabs.librarySystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
